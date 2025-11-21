86e7dee2305dac8f50ca4475bd887df89b921f62
cp-20 <n.naoki.1587@gmail.com>
2025-11-22 00:47:21 +0900

    twitter ogp

--- components.tsx
+++ components.tsx
@@ -13,6 +13,9 @@ export const Main: FC<Props> = ({ counter }) => {
         <title>:awoo:</title>
         <meta property="og:title" content=":awoo:" />
         <meta property="og:image" content="https://awoo.trap.show/awoo.webp" />
+        <meta name="twitter:card" content="summary_image" />
+        <meta name="twitter:title" content=":awoo:" />
+        <meta name="twitter:image" content="https://awoo.trap.show/awoo.webp" />
 
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" />
         <script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.8/dist/htmx.min.js" integrity="sha384-/TgkGk7p307TH7EXJDuUlgG3Ce1UVolAOFopFekQkkXihi5u/6OCvVKyz1W+idaz" crossorigin="anonymous" />
